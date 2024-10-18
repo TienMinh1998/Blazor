db608a473d02806c3bf7bc07726b01b60362f24b
tienminh <lamthuc1998@gmail.com>
2024-10-18 10:27:43 +0700

    adding the pass

--- THFontEnd/Models/LoginModelRequest.cs
+++ THFontEnd/Models/LoginModelRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace THFontEnd.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace THFontEnd.Models
 {
 	public class LoginModelRequest
 	{
-        public string UserName { get; set; }
-        public string Passwork { get; set; }
+		[Required(ErrorMessage = "Không được để trống tên tài khoản")]
+		public string UserName { get; set; }
+		[Required(ErrorMessage = "Không được để trống mật khẩu")]
+		public string Passwork { get; set; }
     }
 }
